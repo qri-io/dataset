8e2752f82be3e743ad7a80e77c9e5c199d96393f
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-08-08 15:37:24 -0400

    add UnmarshalDataset func

--- dataset.go
+++ dataset.go
@@ -1,5 +1,10 @@
 package dataset
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Current version of the specification
 const version = "0.0.1"
 
@@ -7,4 +12,21 @@ const version = "0.0.1"
 type Dataset struct {
 	Metadata
 	Resource
+}
+
+// UnmarshalResource tries to extract a resource type from an empty
+// interface. Pairs nicely with datastore.Get() from github.com/ipfs/go-datastore
+func UnmarshalDataset(v interface{}) (*Dataset, error) {
+	switch r := v.(type) {
+	case *Dataset:
+		return r, nil
+	case Dataset:
+		return &r, nil
+	case []byte:
+		dataset := &Dataset{}
+		err := json.Unmarshal(r, dataset)
+		return dataset, err
+	default:
+		return nil, fmt.Errorf("couldn't parse dataset")
+	}
 }
