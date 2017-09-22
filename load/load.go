80176fd2632ed9dae86418877fba0623746354eb
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-09-22 18:13:23 -0400

    cafs now accepts files

--- load/load.go
+++ load/load.go
@@ -2,12 +2,12 @@
 package load
 
 import (
-	"bytes"
 	"encoding/csv"
 	"fmt"
 	"github.com/qri-io/cafs"
 	"github.com/qri-io/dataset"
 	"github.com/qri-io/dataset/writers"
+	"io"
 )
 
 // RowDataRows loads a slice of raw bytes inside a limit/offset row range
@@ -18,7 +18,7 @@ func RawDataRows(store cafs.Filestore, ds *dataset.Dataset, limit, offset int) (
 	// 	return nil, err
 	// }
 
-	rawdata, err := ds.LoadData(store)
+	datafile, err := ds.LoadData(store)
 	if err != nil {
 		return nil, err
 	}
@@ -32,7 +32,7 @@ func RawDataRows(store cafs.Filestore, ds *dataset.Dataset, limit, offset int) (
 	// w := csv.NewWriter(buf)
 	w := writers.NewWriter(st)
 
-	err = EachRow(st, rawdata, func(i int, row [][]byte, err error) error {
+	err = EachRow(st, datafile, func(i int, row [][]byte, err error) error {
 		if err != nil {
 			return err
 		} else if i < offset {
@@ -59,10 +59,10 @@ func RawDataRows(store cafs.Filestore, ds *dataset.Dataset, limit, offset int) (
 type DataIteratorFunc func(int, [][]byte, error) error
 
 // EachRow calls fn on each row of raw data, using a structure for parsing
-func EachRow(st *dataset.Structure, rawdata []byte, fn DataIteratorFunc) error {
+func EachRow(st *dataset.Structure, r io.Reader, fn DataIteratorFunc) error {
 	switch st.Format {
 	case dataset.CsvDataFormat:
-		rdr := csv.NewReader(bytes.NewReader(rawdata))
+		rdr := csv.NewReader(r)
 		if HeaderRow(st) {
 			if _, err := rdr.Read(); err != nil {
 				if err.Error() == "EOF" {
@@ -108,16 +108,16 @@ func AllRows(store cafs.Filestore, ds *dataset.Dataset) (data [][][]byte, err er
 	// 	return nil, err
 	// }
 
-	rawdata, err := ds.LoadData(store)
+	datafile, err := ds.LoadData(store)
 	if err != nil {
 		return nil, err
 	}
 
-	return FormatRows(ds.Structure, rawdata)
+	return FormatRows(ds.Structure, datafile)
 }
 
-func FormatRows(st *dataset.Structure, rawdata []byte) (data [][][]byte, err error) {
-	err = EachRow(st, rawdata, func(_ int, row [][]byte, e error) error {
+func FormatRows(st *dataset.Structure, file io.Reader) (data [][][]byte, err error) {
+	err = EachRow(st, file, func(_ int, row [][]byte, e error) error {
 		if e != nil {
 			return e
 		}
