62a1233fb681691a7a1340925a571b8b65b6a97b
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-09-26 08:40:18 -0400

    temp revial of dsutil package, need tests

--- dsutil/dsutil.go
+++ dsutil/dsutil.go
@@ -1,19 +1,81 @@
 // Dataset util funcs, placed here to avoid dataset package bloat
 package dsutil
 
-// import (
-// 	"archive/zip"
-// 	"bytes"
-// 	"encoding/json"
-// 	"fmt"
-// 	"io"
-// 	"io/ioutil"
-// 	"path/filepath"
+import (
+	"archive/zip"
+	"encoding/json"
+	"fmt"
+	"github.com/qri-io/cafs"
+	"github.com/qri-io/dataset"
+	"github.com/qri-io/dataset/dsfs"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
-// 	"github.com/ipfs/go-datastore"
-// 	"github.com/qri-io/dataset"
-// 	"github.com/qri-io/fs"
-// )
+func WriteZipArchive(store cafs.Filestore, ds *dataset.Dataset, w io.Writer) error {
+	zw := zip.NewWriter(w)
+
+	dsf, err := zw.Create(dsfs.PackageFileDataset.String())
+	if err != nil {
+		return err
+	}
+	dsdata, err := json.MarshalIndent(ds, "", "  ")
+	if err != nil {
+		return err
+	}
+	_, err = dsf.Write(dsdata)
+	if err != nil {
+		return err
+	}
+
+	datadst, err := zw.Create(fmt.Sprintf("data.%s", ds.Structure.Format.String()))
+	if err != nil {
+		return err
+	}
+
+	datasrc, err := dsfs.LoadDatasetData(store, ds)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(datadst, datasrc); err != nil {
+		return err
+	}
+
+	return zw.Close()
+}
+
+func WriteDir(store cafs.Filestore, ds *dataset.Dataset, path string) error {
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return err
+	}
+
+	dsdata, err := json.MarshalIndent(ds, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filepath.Join(path, dsfs.PackageFileDataset.String()), dsdata, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	datasrc, err := dsfs.LoadDatasetData(store, ds)
+	if err != nil {
+		return err
+	}
+
+	datadst, err := os.Create(filepath.Join(path, fmt.Sprintf("data.%s", ds.Structure.Format.String())))
+	if err != nil {
+		return err
+	}
+	if _, err = io.Copy(datadst, datasrc); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // // PackageDataset creates a zip archive from a store & address
 // func PackageDataset(store datastore.Datastore, r *dataset.Dataset) (io.ReaderAt, int64, error) {
