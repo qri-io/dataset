187f2964e550e3cb9a7b422f45248dad88b8619e
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-11-22 08:56:06 -0500

    test(dsutil,DataFormat): more test coverage

--- dsutil/dsutil.go
+++ dsutil/dsutil.go
@@ -5,25 +5,25 @@ import (
 	"archive/zip"
 	"encoding/json"
 	"fmt"
-	"github.com/ipfs/go-datastore"
-	"github.com/qri-io/cafs"
-	"github.com/qri-io/dataset"
-	"github.com/qri-io/dataset/dsfs"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/qri-io/cafs"
+	"github.com/qri-io/dataset"
+	"github.com/qri-io/dataset/dsfs"
 )
 
 // TODO - make sure a provided path is valid
-func ValidPath(path datastore.Key) (datastore.Key, error) {
-	return path, nil
-}
+// func ValidPath(path datastore.Key) (datastore.Key, error) {
+// 	return path, nil
+// }
 
 // TODO - clean & find valid path to dataset
-func ValidDatasetPath(path datastore.Key) (datastore.Key, error) {
-	return path, nil
-}
+// func ValidDatasetPath(path datastore.Key) (datastore.Key, error) {
+// 	return path, nil
+// }
 
 func WriteZipArchive(store cafs.Filestore, ds *dataset.Dataset, w io.Writer) error {
 	zw := zip.NewWriter(w)
--- dsutil/dsutil_test.go
+++ dsutil/dsutil_test.go
@@ -1,60 +1,119 @@
 package dsutil
 
-// import (
-// 	"archive/zip"
-// 	// "os"
-// 	// "path/filepath"
+import (
+	"archive/zip"
+	"bytes"
+	"github.com/ipfs/go-datastore"
+	"github.com/qri-io/dataset/datatypes"
+	"github.com/qri-io/dataset/dsfs"
+	"os"
+	"path/filepath"
+	"testing"
 
-// 	"github.com/qri-io/dataset"
-// 	"github.com/qri-io/fs/local"
+	"github.com/qri-io/cafs"
+	"github.com/qri-io/cafs/memfs"
+	"github.com/qri-io/dataset"
+)
 
-// 	"testing"
-// )
+func TestWriteZipArchive(t *testing.T) {
+	store, names, err := testStore()
+	if err != nil {
+		t.Errorf("error creating store: %s", err.Error())
+		return
+	}
 
-// func TestPackageDataset(t *testing.T) {
-// 	// wd, err := os.Getwd()
-// 	// if err != nil {
-// 	// 	t.Errorf("error reading working directory: %s", err.Error())
-// 	// 	return
-// 	// }
-// 	store := local.NewLocalStore("test_data")
+	ds, err := dsfs.LoadDataset(store, names["movies"])
+	if err != nil {
+		t.Errorf("error fetching movies dataset from store: %s", err.Error())
+		return
+	}
 
-// 	data, err := store.Read("local/b/dataset.json")
-// 	if err != nil {
-// 		t.Errorf("error reading b/dataset.json: %s", err.Error())
-// 		return
-// 	}
+	buf := &bytes.Buffer{}
+	if err = WriteZipArchive(store, ds, buf); err != nil {
+		t.Errorf("error writing zip archive: %s", err.Error())
+		return
+	}
 
-// 	ds := &dataset.Dataset{}
-// 	if err := ds.UnmarshalJSON(data); err != nil {
-// 		t.Errorf("error unmarshalling b/dataset.json: %s", err.Error())
-// 		return
-// 	}
+	zr, err := zip.NewReader(bytes.NewReader(buf.Bytes()), int64(buf.Len()))
+	if err != nil {
+		t.Errorf("error creating zip reader: %s", err.Error())
+		return
+	}
 
-// 	r, size, err := PackageDataset(store, ds)
-// 	// r, size, err := ns.Package(dataset.NewAddress("local.b"))
-// 	if err != nil {
-// 		t.Errorf("error packaging dataset: %s", err.Error())
-// 		return
-// 	}
+	for _, f := range zr.File {
+		rc, err := f.Open()
+		if err != nil {
+			t.Errorf("error opening file %s in package", f.Name)
+			break
+		}
 
-// 	zr, err := zip.NewReader(r, size)
-// 	if err != nil {
-// 		t.Errorf("error creating zip reader: %s", err.Error())
-// 		return
-// 	}
+		if err := rc.Close(); err != nil {
+			t.Errorf("error closing file %s in package", f.Name)
+			break
+		}
+	}
+}
 
-// 	for _, f := range zr.File {
-// 		// fmt.Println(f.Name)
-// 		rc, err := f.Open()
-// 		if err != nil {
-// 			t.Errorf("error opening file %s in package", f.Name)
-// 			break
-// 		}
+func TestWriteDir(t *testing.T) {
+	store, names, err := testStore()
+	if err != nil {
+		t.Errorf("error creating store: %s", err.Error())
+		return
+	}
 
-// 		if err := rc.Close(); err != nil {
-// 			t.Errorf("error closing file %s in package", f.Name)
-// 			break
-// 		}
-// 	}
-// }
+	ds, err := dsfs.LoadDataset(store, names["movies"])
+	if err != nil {
+		t.Errorf("error fetching movies dataset from store: %s", err.Error())
+		return
+	}
+
+	dir := filepath.Join(os.TempDir(), "dsutil_test_write_dir")
+	if err = os.MkdirAll(dir, os.ModePerm); err != nil {
+		t.Errorf("error creating temp directory: %s", err.Error())
+		return
+	}
+
+	if err = WriteDir(store, ds, dir); err != nil {
+		t.Errorf("error writing directory: %s", err.Error())
+		return
+	}
+
+	// TODO - check files in directory are clean
+
+	if err = os.RemoveAll(dir); err != nil {
+		t.Errorf("error cleaning up after writeDir test: %s", err.Error())
+		return
+	}
+}
+
+func testStore() (cafs.Filestore, map[string]datastore.Key, error) {
+	fs := memfs.NewMapstore()
+	ns := map[string]datastore.Key{
+		"movies": datastore.NewKey(""),
+	}
+
+	datakey, err := fs.Put(memfs.NewMemfileBytes("testdata/movies.csv", []byte("movie\nup\nthe incredibles")), true)
+	if err != nil {
+		return fs, ns, err
+	}
+
+	ds := &dataset.Dataset{
+		Structure: &dataset.Structure{
+			Format: dataset.CsvDataFormat,
+			Schema: &dataset.Schema{
+				Fields: []*dataset.Field{
+					&dataset.Field{Name: "movie", Type: datatypes.String},
+				},
+			},
+		},
+		Data: datakey,
+	}
+
+	dskey, err := dsfs.SaveDataset(fs, ds, true)
+	if err != nil {
+		return fs, ns, err
+	}
+	ns["movies"] = dskey
+
+	return fs, ns, nil
+}
