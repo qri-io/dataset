2f9289fd064c09bbdbf8cf8b2c379d6d4d41cbbc
b5 <sparkle_pony_2000@qri.io>
2018-05-18 10:55:48 -0400

    fix(detect.JSONSchema): fix bufio overflow when detecting on large files
    I ran into this issue when trying to add a 1Gig dataset:
    `error determining dataset schema: error reading data: bufio: buffer full`
    so it's high-time we replace this stupid bufio thing.

--- detect/json.go
+++ detect/json.go
@@ -1,8 +1,6 @@
 package detect
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 
@@ -11,17 +9,31 @@ import (
 )
 
 // JSONSchema determines the field names and types of an io.Reader of JSON-formatted data, returning a json schema
+// This is currently a suuuuuuuuper simple interpretation that spits out a generic schema that'll work. In the future
+// we can do all sorts of stuff here to make better inferences about the shape of a dataset, but for now, this'll work,
+// and we'll instead focus on making it easier for users to provide hand-built schemas
 func JSONSchema(resource *dataset.Structure, data io.Reader) (schema *jsonschema.RootSchema, err error) {
-	rd := bufio.NewReader(data)
-	lin, err := rd.ReadSlice('{')
-	if err != nil && err != io.EOF {
-		log.Debugf(err.Error())
-		return nil, fmt.Errorf("error reading data: %s", err.Error())
-	}
+	buf := make([]byte, 100)
+	for {
+		if _, err := data.Read(buf); err != nil {
+			if err == io.EOF {
+				return nil, fmt.Errorf("invalid json data")
+			}
+			log.Debugf(err.Error())
+			return nil, fmt.Errorf("error reading data: %s", err.Error())
+		}
 
-	if bytes.Contains(lin, []byte{'['}) {
-		return dataset.BaseSchemaArray, nil
+		for _, b := range buf {
+			switch b {
+			case '[':
+				return dataset.BaseSchemaArray, nil
+			case '{':
+				return dataset.BaseSchemaObject, nil
+			case ' ', '\t', '\n', '\r':
+				continue
+			default:
+				return nil, fmt.Errorf("invalid json data")
+			}
+		}
 	}
-
-	return dataset.BaseSchemaObject, nil
 }
