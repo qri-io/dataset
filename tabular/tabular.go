9fec0a32f7494884f01a3663beb432b76910f60d
b5 <sparkle_pony_2000@qri.io>
2020-03-23 11:32:04 -0400

    feat(tabular): package tabular defines tools for tabular datasets

---
+++ tabular/tabular.go
@@ -0,0 +1,197 @@
+// Package tabular defines functions for working with rectangular datasets.
+// qri positions tabular data as a special shape that comes with additional
+// constraints. This package defines the methods necessary to enforce and
+// interpret those conststraints
+package tabular
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// ErrInvalidTabularSchema is a base type for schemas that don't work as tables
+// all parsing errors in this package can be errors.Is() to this one
+var ErrInvalidTabularSchema = errors.New("invalid tabular schema")
+
+// Columns is an ordered list of column information
+type Columns []Column
+
+// Titles gives just column titles as a slice of strings
+func (cols Columns) Titles() []string {
+	titles := make([]string, len(cols))
+	for i, col := range cols {
+		titles[i] = col.Title
+	}
+	return titles
+}
+
+// ValidMachineTitles confirms column titles are valid for machine-readability
+// using column titles that parse as proper variable names, and unique titles
+// across the column set
+func (cols Columns) ValidMachineTitles() error {
+	re := regexp.MustCompile(`^[a-zA-Z_$][a-zA-Z_$0-9]*$`)
+
+	var problems []string
+	set := map[string]struct{}{}
+
+	for i, col := range cols {
+		t := col.Title
+		if !re.MatchString(t) {
+			problems = append(problems, fmt.Sprintf("col. %d name '%s' is not a valid column name", i, t))
+		}
+		if _, present := set[t]; present {
+			problems = append(problems, fmt.Sprintf("col. %d name '%s' is not unique", i, t))
+		}
+		set[t] = struct{}{}
+	}
+
+	if len(problems) > 0 {
+		return fmt.Errorf("%w: column names have problems:\n%s", ErrInvalidTabularSchema, strings.Join(problems, "\n"))
+	}
+	return nil
+}
+
+// Column defines values associated with an index of each row of data
+type Column struct {
+	Title       string                 `json:"title"`
+	Type        *ColType               `json:"type"`
+	Description string                 `json:"description,omitempty"`
+	Validation  map[string]interface{} `json:"validation,omitempty"`
+}
+
+// ColType implements type information for a tablular column. Column Types can
+// be one or more strings enumerating accepted types
+type ColType []string
+
+// MarshalJSON encodes to string in the common case of a single type, an array
+// of strings for a type enumeration
+func (ct ColType) MarshalJSON() ([]byte, error) {
+	switch len(ct) {
+	case 0:
+		return nil, nil
+	case 1:
+		return json.Marshal(ct[0])
+	default:
+		return json.Marshal([]string(ct))
+	}
+}
+
+// UnmarshalJSON decodes string and string array data types
+func (ct *ColType) UnmarshalJSON(p []byte) error {
+	var str string
+	if err := json.Unmarshal(p, &str); err == nil {
+		*ct = ColType{str}
+		return nil
+	}
+
+	var strs []string
+	if err := json.Unmarshal(p, &strs); err == nil {
+		*ct = ColType(strs)
+		return nil
+	}
+
+	return fmt.Errorf("invalid data for ColType")
+}
+
+// ColumnsFromJSONSchema extrats column data from a jsonSchema object, erroring
+// if the provided schema cannot be used to describe a table. a slice of problem
+// strings describes non-breaking issues with the schema that should be
+// addressed like missing column titles or column types
+// the passed in schema must be a decoding of a json schema into default type
+// mappings from the encoding/json package
+func ColumnsFromJSONSchema(sch map[string]interface{}) (Columns, []string, error) {
+	topLevelType, ok := sch["type"].(string)
+	if !ok {
+		msg := "top-level 'type' field is required"
+		return nil, nil, fmt.Errorf("%w: %s", ErrInvalidTabularSchema, msg)
+	}
+
+	switch topLevelType {
+	case "array":
+		return arrayWrapperColumns(sch)
+	case "object":
+		return objectWrapperColumns(sch)
+	default:
+		msg := fmt.Sprintf("'%s' is not a valid type to describe the top level of a tablular schema", topLevelType)
+		return nil, nil, fmt.Errorf("%w: %s", ErrInvalidTabularSchema, msg)
+	}
+}
+
+func arrayWrapperColumns(sch map[string]interface{}) (Columns, []string, error) {
+	var problems []string
+
+	itemObj, ok := sch["items"].(map[string]interface{})
+	if !ok {
+		msg := "top level 'items' property must be an object"
+		return nil, nil, fmt.Errorf("%w: %s", ErrInvalidTabularSchema, msg)
+	}
+
+	itemArr, ok := itemObj["items"].([]interface{})
+	if !ok {
+		msg := "items.items must be an array"
+		return nil, nil, fmt.Errorf("%w: %s", ErrInvalidTabularSchema, msg)
+	}
+
+	cols := make([]Column, len(itemArr))
+	for i, f := range itemArr {
+		// set defaults that
+		cols[i].Title = fmt.Sprintf("col_%d", i)
+		cols[i].Type = &ColType{"string"}
+
+		colSchema, ok := f.(map[string]interface{})
+		if !ok {
+			problems = append(problems, fmt.Sprintf("col. %d schema should be an object", i))
+			continue
+		}
+
+		setTitle, setType := false, false
+		for key, val := range colSchema {
+			switch key {
+			case "title":
+				if title, ok := val.(string); ok {
+					setTitle = true
+					cols[i].Title = title
+				}
+			case "type":
+				setType = true
+				switch x := val.(type) {
+				case string:
+					cols[i].Type = &ColType{x}
+				case []interface{}:
+					types := ColType{}
+					for _, v := range x {
+						if t, ok := v.(string); ok {
+							types = append(types, t)
+						}
+					}
+					cols[i].Type = &types
+				}
+			case "description":
+				if d, ok := val.(string); ok {
+					cols[i].Description = d
+				}
+			default:
+				if cols[i].Validation == nil {
+					cols[i].Validation = map[string]interface{}{}
+				}
+				cols[i].Validation[key] = val
+			}
+		}
+
+		if !setTitle {
+			problems = append(problems, fmt.Sprintf("col. %d title is not set", i))
+		}
+		if !setType {
+			problems = append(problems, fmt.Sprintf("col, %d type is not set, defaulting to string", i))
+		}
+	}
+
+	return cols, problems, nil
+}
+
+func objectWrapperColumns(sch map[string]interface{}) (Columns, []string, error) {
+	return nil, nil, fmt.Errorf("unfinished")
+}
---
+++ tabular/tabular_test.go
@@ -0,0 +1,218 @@
+package tabular
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestColumnsFromJSONSchema(t *testing.T) {
+	good := []struct {
+		description string
+		input       string
+		expect      Columns
+	}{
+		{"one column array wrapper", `{
+			"type" : "array",
+			"items": {
+				"type": "array",
+				"items": [
+					{ "title" : "column_1", "type": "string", "description":"the first column" }
+				]
+			}
+		}`, Columns{
+			{Title: "column_1", Description: "the first column", Type: &ColType{"string"}},
+		}},
+		{"one column array wrapper with validation", `{
+			"type" : "array",
+			"items": {
+				"type": "array",
+				"items": [
+					{ "title" : "rating", "type": ["number", "null"], "description":"0-5 rating", "max": 5, "min": 0 }
+				]
+			}
+		}`, Columns{
+			{Title: "rating", Description: "0-5 rating", Type: &ColType{"number", "null"}, Validation: map[string]interface{}{"max": float64(5), "min": float64(0)}},
+		}},
+		// {"one column object wrapper", `{
+		// 	"type": "object",
+		// 	"properties": {
+		// 		"column_1": {
+		// 			"type": "array",
+		// 			"title": "column_1",
+		// 			"description": "the first column",
+		// 			"items": { "type": "string" }
+		// 		}
+		// 	}
+		// }`, []Column{
+		// 	{Title: "column_1", Description: "the first column", Type: "string"},
+		// }},
+	}
+
+	for _, c := range good {
+		t.Run(c.description, func(t *testing.T) {
+			input := map[string]interface{}{}
+			if err := json.Unmarshal([]byte(c.input), &input); err != nil {
+				t.Fatal(err)
+			}
+
+			got, problems, err := ColumnsFromJSONSchema(input)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if len(problems) != 0 {
+				t.Errorf("unexpected problems: %s", problems)
+			}
+
+			if diff := cmp.Diff(c.expect, got); diff != "" {
+				t.Errorf("result mismatch. (-want +got):\n%s", diff)
+			}
+		})
+	}
+
+	bad := []struct {
+		input string
+		err   string
+	}{
+		{`{}`, "invalid tabular schema: top-level 'type' field is required"},
+		{`{ "type": "string" }`, "invalid tabular schema: 'string' is not a valid type to describe the top level of a tablular schema"},
+		{`{ "type": "array" }`, "invalid tabular schema: top level 'items' property must be an object"},
+		{`{ "type": "array", "items": { "type" : "string" }}`, "invalid tabular schema: items.items must be an array"},
+	}
+	for _, c := range bad {
+		t.Run(fmt.Sprintf("bad_case_%s", c.err), func(t *testing.T) {
+			input := map[string]interface{}{}
+			if err := json.Unmarshal([]byte(c.input), &input); err != nil {
+				t.Fatal(err)
+			}
+
+			_, _, err := ColumnsFromJSONSchema(input)
+			if err == nil {
+				t.Fatal("expected error, got nil")
+			}
+
+			if diff := cmp.Diff(c.err, err.Error()); diff != "" {
+				t.Errorf("result mismatch. (-want +got):\n%s", diff)
+			}
+		})
+	}
+
+	problems := []struct {
+		description string
+		input       string
+		problems    []string
+	}{
+		{"false column",
+			`{ "type": "array", "items": { "type" : "string", "items": [false] }}`,
+			[]string{"col. 0 schema should be an object"},
+		},
+		{"missing title",
+			`{ "type": "array", "items": { "type" : "string", "items": [{"type": "string"}] }}`,
+			[]string{"col. 0 title is not set"},
+		},
+		{"missing type",
+			`{ "type": "array", "items": { "type" : "string", "items": [{"title": "a_column"}] }}`,
+			[]string{"col, 0 type is not set, defaulting to string"},
+		},
+	}
+	for _, c := range problems {
+		t.Run(fmt.Sprintf("problem_%s", c.description), func(t *testing.T) {
+			input := map[string]interface{}{}
+			if err := json.Unmarshal([]byte(c.input), &input); err != nil {
+				t.Fatal(err)
+			}
+
+			_, problems, err := ColumnsFromJSONSchema(input)
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if diff := cmp.Diff(c.problems, problems); diff != "" {
+				t.Errorf("result mismatch. (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestColumnsJSON(t *testing.T) {
+	val := `[{"title":"foo","type":["string","number"]},{"title":"bar","type":"string"}]`
+	cols := &Columns{}
+	if err := json.Unmarshal([]byte(val), cols); err != nil {
+		t.Errorf("unmarshal error: %s", err)
+	}
+
+	data, err := json.Marshal(cols)
+	if err != nil {
+		t.Errorf("marshal error: %s", err)
+	}
+	
+	if diff := cmp.Diff(val, string(data)); diff != "" {
+		t.Errorf("result mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestColumnsTitles(t *testing.T) {
+	cols := Columns{
+		Column{Title: "foo"},
+		Column{Title: ""},
+		Column{Title: "🔥"},
+	}
+
+	got := cols.Titles()
+	expect := []string{"foo", "", "🔥"}
+	if diff := cmp.Diff(expect, got); diff != "" {
+		t.Errorf("result mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestValidMachineTitles(t *testing.T) {
+	good := []struct {
+		description string
+		cols        Columns
+	}{
+		{"single title",
+			Columns{
+				Column{Title: "foo"},
+			},
+		},
+	}
+	for _, c := range good {
+		t.Run(c.description, func(t *testing.T) {
+			if err := c.cols.ValidMachineTitles(); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+
+	bad := []struct {
+		err  string
+		cols Columns
+	}{
+		{"invalid tabular schema: column names have problems:\ncol. 0 name '???' is not a valid column name",
+			Columns{
+				Column{Title: "???"},
+			},
+		},
+		{"invalid tabular schema: column names have problems:\ncol. 1 name 'a' is not unique",
+			Columns{
+				Column{Title: "a"},
+				Column{Title: "a"},
+			},
+		},
+	}
+
+	for _, c := range bad {
+		t.Run(c.err, func(t *testing.T) {
+			err := c.cols.ValidMachineTitles()
+			if err == nil {
+				t.Error("expected error, got nil")
+			}
+			if diff := cmp.Diff(c.err, err.Error()); diff != "" {
+				t.Errorf("error mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
